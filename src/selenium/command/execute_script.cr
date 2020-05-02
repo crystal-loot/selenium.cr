556a51b077a220377307e68b07c9dfcb25afc660
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-02 15:46:42 -0500

    Can execute script

--- src/selenium/command/execute_script.cr
+++ src/selenium/command/execute_script.cr
@@ -1,6 +1,16 @@
 class Selenium::Command::ExecuteScript
-  def initialize(@session_id : UUID)
-    @method = "POST"
-    @route = "/session/#{@session_id}/execute/sync"
+  def initialize(@driver : Driver::Postable, @session_id : SessionId)
+  end
+
+  def execute(script : String, args : Array(_) = [] of String) : String
+    response_body = @driver.post(
+      "/session/#{@session_id}/execute/sync",
+      body: {
+        script: script,
+        args:   args,
+      }.to_json
+    )
+
+    response_body["value"].as_s
   end
 end
