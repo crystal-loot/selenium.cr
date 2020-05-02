cefc601feb28ba9b495b05ac3bbfda112e38c5b4
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-01 22:02:21 -0500

    can make a new window

---
+++ spec/selenium/command/new_window_spec.cr
@@ -0,0 +1,21 @@
+require "../../spec_helper"
+
+module Selenium::Command
+  describe NewWindow do
+    it "works" do
+      driver = TestDriver.new
+      driver.response_body = {
+        handle: "abc",
+        type: "tab"
+      }.to_json
+      session_id = SessionId.random
+      command = NewWindow.new(driver, session_id)
+
+      result = command.execute
+
+      driver.request_path.should eq("/session/#{session_id}/window/new")
+      result.handle.should eq("abc")
+      result.type.should eq("tab")
+    end
+  end
+end
--- src/selenium/command/new_window.cr
+++ src/selenium/command/new_window.cr
@@ -1,6 +1,16 @@
 class Selenium::Command::NewWindow
-  def initialize(@session_id : UUID)
-    @method = "POST"
-    @route = "/session/#{@session_id}/windows/new"
+  def initialize(@driver : Driver::Postable, @session_id : SessionId)
+  end
+
+  def execute : Result
+    response_body = @driver.post("/session/#{@session_id}/window/new")
+
+    Result.from_json(response_body)
+  end
+
+  struct Result
+    include JSON::Serializable
+    property handle : String
+    property type : String
   end
 end
