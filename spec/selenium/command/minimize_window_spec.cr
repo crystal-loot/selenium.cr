f00c02c794b66a7c588244ff3c6f83ed60afc418
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-01 22:47:56 -0500

    can minimize window

---
+++ spec/selenium/command/minimize_window_spec.cr
@@ -0,0 +1,25 @@
+require "../../spec_helper"
+
+module Selenium::Command
+  describe MinimizeWindow do
+    it "works" do
+      driver = TestDriver.new
+      driver.response_body = {
+        x: 100,
+        y: 120,
+        width: 400,
+        height: 660
+      }.to_json
+      session_id = SessionId.random
+      command = MinimizeWindow.new(driver, session_id)
+
+      result = command.execute
+
+      driver.request_path.should eq("/session/#{session_id}/window/minimize")
+      result.x.should eq(100)
+      result.y.should eq(120)
+      result.width.should eq(400)
+      result.height.should eq(660)
+    end
+  end
+end
--- src/selenium/command/minimize_window.cr
+++ src/selenium/command/minimize_window.cr
@@ -1,6 +1,9 @@
 class Selenium::Command::MinimizeWindow
-  def initialize(@session_id : UUID)
-    @method = "POST"
-    @route = "/session/#{@session_id}/window/minimize"
+  def initialize(@driver : Driver::Postable, @session_id : SessionId)
+  end
+
+  def execute : WindowRect
+    response_body = @driver.post("/session/#{@session_id}/window/minimize")
+    WindowRect.from_json(response_body)
   end
 end
