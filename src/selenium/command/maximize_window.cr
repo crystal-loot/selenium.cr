b75af8f482d3dd470e372dfcd05f25f893db57ce
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-01 22:46:42 -0500

    can maximize window

--- src/selenium/command/maximize_window.cr
+++ src/selenium/command/maximize_window.cr
@@ -1,6 +1,9 @@
 class Selenium::Command::MaximizeWindow
-  def initialize(@session_id : UUID)
-    @method = "POST"
-    @route = "/session/#{@session_id}/window/maximize"
+  def initialize(@driver : Driver::Postable, @session_id : SessionId)
+  end
+
+  def execute : WindowRect
+    response_body = @driver.post("/session/#{@session_id}/window/maximize")
+    WindowRect.from_json(response_body)
   end
 end
