0de405ddcecb43503277c0e64475e1624bfde541
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-02 12:30:42 -0500

    Can determine if element is selected

--- src/selenium/command/is_element_selected.cr
+++ src/selenium/command/is_element_selected.cr
@@ -1,6 +1,10 @@
 class Selenium::Command::IsElementSelected
-  def initialize(@session_id : UUID, @element_id)
-    @method = "GET"
-    @route = "/session/#{@session_id}/element/#{@element_id}/selected"
+  def initialize(@driver : Driver::Getable, @session_id : SessionId)
+  end
+
+  def execute(element_id : ElementId) : Bool
+    response_body = @driver.get("/session/#{@session_id}/element/#{element_id}/selected")
+
+    response_body["value"].as_bool
   end
 end
