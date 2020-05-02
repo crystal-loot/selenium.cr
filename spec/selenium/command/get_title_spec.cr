04e55ba1d691bf4b8cc8c1d51b43e80ad198c964
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-01 21:40:21 -0500

    can get title

---
+++ spec/selenium/command/get_title_spec.cr
@@ -0,0 +1,15 @@
+require "../../spec_helper"
+
+module Selenium::Command
+  describe GetTitle do
+    it "works" do
+      driver = TestDriver.new
+      driver.response_body = { title: "Foo" }.to_json
+      session_id = SessionId.random
+      command = GetTitle.new(driver, session_id)
+
+      command.execute.should eq("Foo")
+      driver.request_path.should eq("/session/#{session_id}/title")
+    end
+  end
+end
--- src/selenium/command/get_title.cr
+++ src/selenium/command/get_title.cr
@@ -1,6 +1,11 @@
 class Selenium::Command::GetTitle
-  def initialize(@session_id : UUID)
+  def initialize(@driver : Driver::Getable, @session_id : UUID)
     @method = "GET"
     @route = "/session/#{@session_id}/title"
+  end
+
+  def execute : String
+    response_body = @driver.get("/session/#{@session_id}/title")
+    JSON.parse(response_body)["title"].as_s
   end
 end
