dcae686ac3f78fdf6d469c25185e5b11268fef03
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-02 23:50:19 -0500

    Fix and verify ExecuteAsyncScript

--- spec/features/document_spec.cr
+++ spec/features/document_spec.cr
@@ -15,14 +15,21 @@ module Selenium::Command
     end
 
     it "can execute scripts" do
-      TestServer.route "/home", "<h1>The Title</h1>"
+      TestServer.route "/home", "<h1 id=\"title\">The Title</h1>"
       driver = HttpDriver.new
 
       with_session(driver) do |session_id|
         NavigateTo.new(driver, session_id).execute("localhost:3002/home")
         result = ExecuteScript.new(driver, session_id).execute("return 1 + 1;")
-
         result.should eq("2")
+
+        # not really an async script but whatever
+        async_script = <<-JS
+        arguments[arguments.length - 1](document.getElementById("title").textContent)
+        JS
+        result = ExecuteAsyncScript.new(driver, session_id).execute(async_script)
+        
+        result.should eq("\"The Title\"")
       end
     end
   end
