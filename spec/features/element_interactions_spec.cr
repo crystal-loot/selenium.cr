2ed7a9a381adb44acd4e70ac5e9c8c87e077ea70
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-02 23:14:51 -0500

    Verify clearing input

--- spec/features/element_interactions_spec.cr
+++ spec/features/element_interactions_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 module Selenium::Command
   describe "element interactions", tags: "feature" do
-    it "can clicks" do
+    it "can click" do
       TestServer.route "/next-page", "<h1>You made it!</h1>"
       TestServer.route "/home", <<-HTML
         <a href="/next-page">Click Me!</a>
@@ -18,6 +18,21 @@ module Selenium::Command
         current_url = GetCurrentUrl.new(driver, session_id).execute
 
         current_url.should eq("http://localhost:3002/next-page")
+      end
+    end
+
+    it "can clear" do
+      TestServer.route "/home", <<-HTML
+        <input type="text" id="name" value="John">
+      HTML
+      driver = HttpDriver.new
+
+      with_session(driver) do |session_id|
+        NavigateTo.new(driver, session_id).execute("localhost:3002/home")
+        element_id = FindElement.new(driver, session_id).execute(using: LocationStrategy::CSS, value: "#name")
+        ElementClear.new(driver, session_id).execute(element_id)
+        value = GetElementAttribute.new(driver, session_id).execute(element_id, "value")
+        value.should be_empty
       end
     end
   end
