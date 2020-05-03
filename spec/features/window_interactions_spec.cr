c3995cc16e0ac19aede72affcffc2910b8720e32
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-02 23:05:43 -0500

    Fix and test GetWindowHandles

--- spec/features/window_interactions_spec.cr
+++ spec/features/window_interactions_spec.cr
@@ -12,6 +12,9 @@ module Selenium::Command
         MaximizeWindow.new(driver, session_id).execute
         window_handle_a = GetWindowHandle.new(driver, session_id).execute
         window_handle_b = NewWindow.new(driver, session_id).execute
+        window_handles = GetWindowHandles.new(driver, session_id).execute
+        window_handles.should contain(window_handle_a)
+        window_handles.should contain(window_handle_b)
         SwitchToWindow.new(driver, session_id).execute(window_handle_a)
         SetWindowRect.new(driver, session_id).execute(width: 100, height: 300)
 
--- src/selenium/command/get_window_handles.cr
+++ src/selenium/command/get_window_handles.cr
@@ -7,6 +7,6 @@ class Selenium::Command::GetWindowHandles
   def execute : Array(WindowHandle)
     response_body = @driver.get("/session/#{@session_id}/window/handles")
 
-    response_body.dig("value", "handles").as_a.map &.as_s
+    response_body["value"].as_a.map &.as_s
   end
 end
