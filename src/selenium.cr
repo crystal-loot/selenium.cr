b26858f9b7c1f3de5a2f4110bc2e77b92e2f90f7
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-04 00:23:40 -0500

    Switching to this new command structure for more flexibility and it copies the ruby library more

--- src/selenium.cr
+++ src/selenium.cr
@@ -2,6 +2,7 @@ require "base64"
 require "json"
 require "uuid"
 require "uuid/json"
+require "./selenium/default_commands"
 require "./selenium/**"
 
 module Selenium
---
+++ src/selenium/command_handler.cr
@@ -0,0 +1,27 @@
+class Selenium::CommandHandler
+  include DefaultCommands
+  getter http_client : HttpClient
+
+  def initialize(@http_client)
+  end
+
+  def execute(command : Symbol, path_variables : Hash(String, String) = {} of String => String, parameters = nil) : JSON::Any
+    method, path = DEFAULT_COMMANDS[command]
+    full_path = path_variables.reduce(path) { |acc, entry| path.sub(entry.first, entry.last) }
+
+    execute(method, full_path, parameters)
+  end
+
+  private def execute(method, full_path, parameters) : JSON::Any
+    case method
+    when :get
+      http_client.get(full_path)
+    when :delete
+      http_client.delete(full_path).as(JSON::Any)
+    when :post
+      http_client.post(full_path, parameters)
+    else
+      raise "Woops!"
+    end
+  end
+end
