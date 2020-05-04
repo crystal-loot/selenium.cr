0540c754a0dacaca1829671fb2c22f5a43ca869d
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-03 23:11:08 -0500

    Converted error handling spec to use higher level objects

--- spec/features/error_handling_spec.cr
+++ spec/features/error_handling_spec.cr
@@ -5,16 +5,13 @@ module Selenium::Command
     it "raises exceptions when trying to find element that does not exist" do
       TestServer.route "/home", "<h1>The Title</h1>"
       driver = Driver.new
-      http_client = driver.http_client
 
       with_session(driver) do |session|
-        session_id = session.id
-        NavigateTo.new(http_client, session_id).execute("localhost:3002/home")
-        command = FindElement.new(http_client, session_id)
+        session.navigate_to("localhost:3002/home")
 
         expected_message = "no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#missing-element\"}"
         expect_raises(Error, expected_message) do
-          command.execute(using: LocationStrategy::CSS, value: "#missing-element")
+          session.find_element(LocationStrategy::CSS, "#missing-element")
         end
       end
     end
@@ -23,39 +20,16 @@ module Selenium::Command
       TestServer.route "/home", "<div id=\"parent\"><a href=\"/about\">Click</a></div>"
       TestServer.route "/about", "<h1>The Title</h1>"
       driver = Driver.new
-      http_client = driver.http_client
 
       with_session(driver) do |session|
-        session_id = session.id
-        NavigateTo.new(http_client, session_id).execute("localhost:3002/home")
-        parent_element_id = FindElement.new(http_client, session_id).execute(LocationStrategy::CSS, "#parent")
-        anchor_id = FindElement.new(http_client, session_id).execute(LocationStrategy::LINK_TEXT, "Click")
-        ElementClick.new(http_client, session_id).execute(anchor_id)
+        session.navigate_to("localhost:3002/home")
+        parent_element = session.find_element(LocationStrategy::CSS, "#parent")
+        child_element = parent_element.find_child_element(LocationStrategy::LINK_TEXT, "Click")
+        child_element.click
 
         expected_message = "stale element reference: element is not attached to the page document"
         expect_raises(Error, expected_message) do
-          FindElementFromElement.new(http_client, session_id, parent_element_id).execute(LocationStrategy::LINK_TEXT, "Click")
-        end
-      end
-    end
-
-    it "raises exceptions when trying to find element within element that no longer exists" do
-      TestServer.route "/home", "<div id=\"parent\"><a href=\"/about\">Click</a></div>"
-      TestServer.route "/about", "<h1>The Title</h1>"
-      driver = Driver.new
-      http_client = driver.http_client
-
-      with_session(driver) do |session|
-        session_id = session.id
-        NavigateTo.new(http_client, session_id).execute("localhost:3002/home")
-        parent_element_id = FindElement.new(http_client, session_id).execute(LocationStrategy::CSS, "#parent")
-        anchor_id = FindElement.new(http_client, session_id).execute(LocationStrategy::LINK_TEXT, "Click")
-        ElementClick.new(http_client, session_id).execute(anchor_id)
-
-        expected_message = "stale element reference: element is not attached to the page document"
-        expect_raises(Error, expected_message) do
-          FindElementFromElement.new(http_client, session_id, parent_element_id)
-            .execute(LocationStrategy::LINK_TEXT, "Click")
+          parent_element.find_child_element(LocationStrategy::LINK_TEXT, "Click")
         end
       end
     end
@@ -63,26 +37,22 @@ module Selenium::Command
     it "raises exception when making get command" do
       TestServer.route "/home", "<h1 id=\"title\">Title</h1>"
       driver = Driver.new
-      http_client = driver.http_client
 
       with_session(driver) do |session|
-        session_id = session.id
-        NavigateTo.new(http_client, session_id).execute("localhost:3002/home")
+        session.navigate_to("localhost:3002/home")
 
         expected_message = "stale element reference: element is not attached to the page document"
         expect_raises(Error, expected_message) do
-          invalid_element_id = ElementId.random
-          GetElementProperty.new(http_client, session_id).execute(invalid_element_id, "id")
+          Element.new(session, ElementId.random).property("id")
         end
       end
     end
 
     it "raises exception when making delete command" do
       driver = Driver.new
-      http_client = driver.http_client
 
       expect_raises(Error, "invalid session id") do
-        CloseWindow.new(http_client, "invalidsessionid").execute
+        Session.new(driver.http_client, "invalidsessionid").window_manager.close_window
       end
     end
   end
