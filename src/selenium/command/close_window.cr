09ddad3d82f537509da9a132785b7bbb97d35063
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-04 19:48:53 -0500

    Removing unused commands

--- src/selenium/command/close_window.cr
+++
@@ -1,8 +0,0 @@
-class Selenium::Command::CloseWindow
-  def initialize(@http_client : HttpClient, @session_id : SessionId)
-  end
-
-  def execute
-    @http_client.delete("/session/#{@session_id}/window")
-  end
-end
