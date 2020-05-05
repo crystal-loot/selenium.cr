3896f48939a1724989ba8ce631ce4f4c33401efe
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-04 19:20:13 -0500

    Remove unused element level commands

--- src/selenium/command/element_clear.cr
+++
@@ -1,8 +0,0 @@
-class Selenium::Command::ElementClear
-  def initialize(@http_client : HttpClient, @session_id : SessionId)
-  end
-
-  def execute(element_id : ElementId)
-    @http_client.post("/session/#{@session_id}/element/#{element_id}/clear")
-  end
-end
--- src/selenium/command/element_click.cr
+++
@@ -1,8 +0,0 @@
-class Selenium::Command::ElementClick
-  def initialize(@http_client : HttpClient, @session_id : SessionId)
-  end
-
-  def execute(element_id : ElementId)
-    @http_client.post("/session/#{@session_id}/element/#{element_id}/click")
-  end
-end
