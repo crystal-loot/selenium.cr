266d9f6b1c3d63be26769c979a77801cdad7a3c0
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-04 19:11:06 -0500

    Remove unused cookie commands

--- src/selenium/command/delete_all_cookies.cr
+++
@@ -1,8 +0,0 @@
-class Selenium::Command::DeleteAllCookies
-  def initialize(@http_client : HttpClient, @session_id : SessionId)
-  end
-
-  def execute
-    @http_client.delete("/session/#{@session_id}/cookie")
-  end
-end
--- src/selenium/command/delete_cookie.cr
+++
@@ -1,8 +0,0 @@
-class Selenium::Command::DeleteCookie
-  def initialize(@http_client : HttpClient, @session_id : SessionId)
-  end
-
-  def execute(name : String)
-    @http_client.delete("/session/#{@session_id}/cookie/#{name}")
-  end
-end
