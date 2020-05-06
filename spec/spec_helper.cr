c26dea5073f9e6fb453d050c3269a6df64c1989a
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-05-05 19:05:53 -0500

    Can run specs with chrome or firefox (FIREFOX WIP)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -17,10 +17,35 @@ spawn do
   server.listen
 end
 
-CHROME_HEADLESS = Selenium::Chrome::Capabilities.new
-CHROME_HEADLESS.args(["--no-sandbox", "--headless", "--disable-gpu"])
+def browser
+  ENV["SELENIUM_BROWSER"]? || "chrome"
+end
 
-def with_session(driver = Selenium::Driver.for(:chrome), capabilities = CHROME_HEADLESS)
+def build_capabilities
+  if browser == "chrome"
+    capabilities = Selenium::Chrome::Capabilities.new
+    capabilities.args(["no-sandbox", "headless", "disable-gpu"])
+    capabilities
+  elsif browser == "firefox"
+    capabilities = Selenium::Firefox::Capabilities.new
+    capabilities.args(["-headless"])
+    capabilities
+  else
+    raise ArgumentError.new("unknown browser for running tests: #{browser}")
+  end
+end
+
+def build_driver
+  if browser == "chrome"
+    Selenium::Driver.for(:chrome)
+  elsif browser == "firefox"
+    Selenium::Driver.for(:firefox, {base_url: "http://localhost:4444"})
+  else
+    raise ArgumentError.new("unknown browser for running tests: #{browser}")
+  end
+end
+
+def with_session(driver = build_driver, capabilities = build_capabilities)
   session = driver.create_session(capabilities)
   yield(session)
 ensure
