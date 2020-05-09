660a2dd0331ffc2f217c326df6e12de3ed1538b2
Matthew McGarvey <tnraptor14@hotmail.com>
2020-05-08 19:18:51 -0500

    Use service in tests

---
+++ spec/support/test_driver_factory.cr
@@ -0,0 +1,34 @@
+class Selenium::TestDriverFactory
+  def self.build(browser) : Tuple(Driver, Capabilities)
+    case browser
+    when "chrome"
+      build_chrome_driver
+    when "firefox"
+      build_firefox_driver
+    else
+      raise ArgumentError.new("unknown browser for running tests: #{browser}")
+    end
+  end
+
+  def self.build_chrome_driver : Tuple(Driver, Capabilities)
+    capabilities = Chrome::Capabilities.new
+    capabilities.args(["no-sandbox", "headless", "disable-gpu"])
+    driver = Driver.for(:chrome, service: Service.chrome(driver_path: Webdrivers::Chromedriver.install))
+    {driver, capabilities}
+  end
+
+  def self.build_firefox_driver : Tuple(Driver, Capabilities)
+    capabilities = Firefox::Capabilities.new
+    capabilities.args(["-headless"])
+    driver = Driver.for(:firefox, service: Service.firefox(driver_path: Webdrivers::Geckodriver.install))
+    {driver, capabilities}
+  end
+
+  private def self.chrome?(browser)
+    browser == "chrome"
+  end
+
+  private def self.firefox?(browser)
+    browser == "firefox"
+  end
+end
