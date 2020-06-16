ede8ccc14bd96c9e81978400805eac8eb684f915
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-06-15 20:51:01 -0500

    Fixed last warning

--- src/selenium/window_rect.cr
+++ src/selenium/window_rect.cr
@@ -1,10 +1,10 @@
 struct Selenium::WindowRect
-  JSON.mapping(
-    x: Int64?,
-    y: Int64?,
-    width: Int64?,
-    height: Int64?
-  )
+  include JSON::Serializable
+
+  property x : Int64?
+  property y : Int64?
+  property width : Int64?
+  property height : Int64?
 
   def initialize(@x = nil, @y = nil, @width = nil, @height = nil)
   end
